261aa8272f4d9bccc0abfff29a0021772c9eaf41
vosid <temirboyev0823@gmail.com>
2023-03-24 23:44:48 +0500

    updated from .toFixed to Math.floor

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const small = document.querySelector('.small'),
-	big = document.querySelector('.big'),
-	preload = document.querySelector('.preload')
+const small = document.querySelector(".small"),
+	big = document.querySelector(".big"),
+	preload = document.querySelector(".preload")
 
 setInterval(() => {
-	let difference = new Date('23 august 2068') - new Date()
-	let mins = (difference / 1000 / 60).toFixed(0)
-	let secs = ((difference / 1000) % 60).toFixed(0)
-	let years = (difference / 1000 / 60 / 60 / 24 / 30.42 / 12).toFixed(0)
-	let months = ((difference / 1000 / 60 / 60 / 24 / 30.42) % 12).toFixed(0)
-	days = ((difference / 1000 / 60 / 60 / 24) % 12).toFixed(0)
+	let difference = new Date("23 august 2068") - new Date()
+	let mins = Math.floor(difference / 1000 / 60)
+
+	let secs = Math.floor((difference / 1000) % 60)
+	let years = Math.floor(difference / 1000 / 60 / 60 / 24 / 30.42 / 12)
+	let months = Math.floor((difference / 1000 / 60 / 60 / 24 / 30.42) % 12)
+	let days = Math.floor((difference / 1000 / 60 / 60 / 24) % 12)
 	small.textContent = `
-	  ${zero(mins.replace(/(?=(?:.{3})*$)/g, ' '))} : ${zero(secs)}
+	  ${zero(mins.toString().replace(/(?=(?:.{3})*$)/g, " "))} : ${zero(secs)}
 	`
-	big.textContent = `${years}y ${zero(months)}m ${zero(days)}d`
-	preload.style.display = 'none'
+
+	big.textContent = `${years}y ${months}m ${days}d`
+	preload.style.display = "none"
 }, 1000)
 
 function zero(x) {
